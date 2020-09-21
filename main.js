88e70d5926056e7717b7dd31386b20a6e8e7650a
Salemar <fireshowel@gmail.com>
2020-09-21 16:55:23 +0300

    updates

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require("path");
-const port = 27036;
-const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
-
-app.get('/', function(req, res) {
+app.set('port', process.env.PORT || 8888);
+app.use(bodyparser.json());
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(port)
+app.listen(8888,'0.0.0.0')
